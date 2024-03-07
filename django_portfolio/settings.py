a683309c33ef997b2d844557c0f070a7f222c92f
tlalky <sebastian03012000@gmail.com>
2024-03-07 14:37:38 +0100

    Hover effect + minor adjustment

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hehe-my-secret-password'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'tlalky.eu.pythonanywhere.com']
 
 
 # Application definition
@@ -127,3 +127,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
