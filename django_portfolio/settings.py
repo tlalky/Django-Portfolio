2c3fbc76e11bacc68fa62ec6a46f75ae570fe315
tlalky <sebastian03012000@gmail.com>
2024-09-30 18:25:19 +0200

    updating home.html and more

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'hehe-my-secret-password'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'tlalky.eu.pythonanywhere.com']
 
